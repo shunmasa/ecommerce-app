c887d3331db9a7c1aa0213f09da7344a01c28885
Macintosh HD <masashilandjob@gmail.com>
2021-10-22 16:37:17 +1300

    snipchart

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import styles from './Header.module.scss'
+import Container from '@components/Container'
+import {FaShoppingCart} from 'react-icons/fa';
+const Header =()=>{
+    return (
+        <header className={styles.header}>
+       <Container className={styles.headerContainer}>
+            <p className={styles.headerTitle}>
+            Hipper Bros. Trading Cards
+            </p>
+            <p className={styles.headerCart}>
+               <button className="snipcart-checkout">
+
+  
+                <FaShoppingCart/>
+                <span className="snipcart-total-price">-</span>
+                </button>
+           </p>
+        </Container>
+        </header>
+    )
+}
+export default Header 
